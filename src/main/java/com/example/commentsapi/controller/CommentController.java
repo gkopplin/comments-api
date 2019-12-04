56a69c90157ad45a98c54416678bdba0715b3a0f
gkopplin <grant.kopplin@gmail.com>
2019-12-03 21:56:29 -0500

    Add edits for demo

--- src/main/java/com/example/commentsapi/controller/CommentController.java
+++ src/main/java/com/example/commentsapi/controller/CommentController.java
@@ -1,10 +1,14 @@
 package com.example.commentsapi.controller;
 
+import com.example.commentsapi.exception.CommentNotFoundException;
+import com.example.commentsapi.exception.PostNotFoundException;
 import com.example.commentsapi.model.Comment;
 import com.example.commentsapi.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class CommentController {
@@ -23,12 +27,12 @@ public class CommentController {
     }
 
     @DeleteMapping("/{id}")
-    public String deleteComment(@PathVariable long id) {
+    public String deleteComment(@PathVariable long id) throws CommentNotFoundException {
         return commentService.deleteComment(id) == HttpStatus.OK ? "success" : "error";
     }
 
     @PostMapping("/{postId}")
-    public Comment createComment(@RequestBody Comment comment, @PathVariable long postId, @RequestHeader("username") String username) throws Exception {
+    public Comment createComment(@Valid @RequestBody Comment comment, @PathVariable long postId, @RequestHeader("username") String username) throws PostNotFoundException {
         return commentService.createComment(comment, postId, username);
     }
 
